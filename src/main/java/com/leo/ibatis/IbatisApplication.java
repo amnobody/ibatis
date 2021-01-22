6995a11d631c22026253f23653146aad2668d2c4
陈继伟 <chenjiweicn@gmail.com>
2021-01-22 14:51:43 +0800

    dev-提交

--- src/main/java/com/leo/ibatis/IbatisApplication.java
+++ src/main/java/com/leo/ibatis/IbatisApplication.java
@@ -1,10 +1,16 @@
 package com.leo.ibatis;
 
+import com.leo.ibatis.web.GlobalExceptionResolve;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
+/**
+ * 排除包扫描
+ */
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.leo.ibatis.*"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {GlobalExceptionResolve.class}))
+@ComponentScan(basePackages = {"com.leo.ibatis.*"},excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,classes = {GlobalExceptionResolve.class}))
 public class IbatisApplication {
 
 	public static void main(String[] args) {
